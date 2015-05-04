5433c69bef6019bf26d5faa2314d2d5ab8d697a5
darkstalker <slayerbeast@gmail.com>
2015-05-04 01:22:46 -0300

    save window position and size

--- src/moonhowl/defaults.lua
+++ src/moonhowl/defaults.lua
@@ -1,8 +1,12 @@
 return {
-    _copy = { accounts = true },
+    _copy = { accounts = true, window = true },
     app_keys = {  -- TOP SECRET!
         consumer_key = "ulQ0DfhkRhW2CrUDIVbKZ3lMw",
         consumer_secret = "vTXo5mFkd4FmMpGU9thpy6os0ygM2CyUPXWh6Cwax2jNVsWflL",
     },
     accounts = {},
+    window = {
+        width = 480,
+        height = 640,
+    },
 }
--- src/moonhowl/gui/main_window.lua
+++ src/moonhowl/gui/main_window.lua
@@ -3,6 +3,7 @@ local Gtk = lgi.Gtk
 local object = require "moonhowl.object"
 local signal = require "moonhowl.signal"
 local ui = require "moonhowl.ui"
+local config = require "moonhowl.config"
 
 local main_window = object:extend()
 
@@ -13,11 +14,13 @@ function main_window:_init(title)
     self.infobar = ui.info_bar:new()
     self.tweet_entry = ui.tweet_entry:new()
 
+    local cw = config.window
     self.handle = Gtk.Window{
         id = "main_window",
-        default_width = 480,
-        default_height = 640,
+        default_width = cw.width,
+        default_height = cw.height,
         on_destroy = Gtk.main_quit,
+        on_configure_event = self:bind(self.handle__on_configure),
 
         Gtk.Box{
             orientation = Gtk.Orientation.VERTICAL,
@@ -48,11 +51,22 @@ function main_window:_init(title)
     signal.listen("ui_set_current_tab", self.signal_set_current_tab, self)
 
     signal.emit "ui_new_tab"  -- action widget not visible until a tab is added
+
+    if cw.x and cw.y then
+        self.handle:move(cw.x, cw.y)
+    end
+
     self.handle:show_all()
 end
 
 function main_window:show_all()
     self.handle:show_all()
+end
+
+function main_window:handle__on_configure(w, ev)
+    local cw = config.window
+    cw.x, cw.y = ev.x, ev.y
+    cw.width, cw.height = ev.width, ev.height
 end
 
 function main_window:signal_compose()
