c5958268bd60a461ef378df55914ea52811bc566
darkstalker <slayerbeast@gmail.com>
2015-06-07 21:56:10 -0300

    added refresh for tweet_list_view

--- src/moonhowl/gui/tweet_list_view.lua
+++ src/moonhowl/gui/tweet_list_view.lua
@@ -9,9 +9,20 @@ function tweet_list_view:_init(content)
 end
 
 function tweet_list_view:add_list(list)
+    self.content = list  -- last received content
     for _, tw in ipairs(list) do
         self:add(ui.tweet_view:new(tw))
     end
+end
+
+function tweet_list_view:refresh()
+    local first_tw = self.content[1]  -- get from sorted list?
+    self.content:_source_method{
+        since_id = first_tw and first_tw.id_str,
+        _callback = function(list)
+            return self:add_list(list)
+        end
+    }
 end
 
 return tweet_list_view
